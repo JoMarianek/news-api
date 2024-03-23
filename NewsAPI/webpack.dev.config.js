c94d05b9c1c0a86321e12f132ce85830b34d9fa1
Joelle Marianek <joelle@LAPTOP-6D50GD4A>
2024-03-23 23:01:14 +0100

    refactor: rename js files to ts files

---
+++ NewsAPI/webpack.dev.config.js
@@ -0,0 +1,9 @@
+"use strict";
+const path = require('path');
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: path.resolve(__dirname, './dist'),
+    },
+};
