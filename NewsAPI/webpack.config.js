b5a738d30275348b0173ab64fd688f9a984988c2
Joelle Marianek <joelle@LAPTOP-6D50GD4A>
2024-03-26 19:36:06 +0100

    refactor: add public path for deployment

--- NewsAPI/webpack.config.js
+++ NewsAPI/webpack.config.js
@@ -26,6 +26,7 @@ const baseConfig = {
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, './dist'),
+        publicPath: './NewsAPI/',
     },
     plugins: [
         new DotenvWebpackPlugin(),
